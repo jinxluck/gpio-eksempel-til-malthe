2e8cc6dec1b9da7bc3801a4220095977327fa4b8
jan k schiermer <janschiermer@gmail.com>
2019-12-19 14:57:41 +0100

    eksempel på gpio i c til pocketbeagle + kommentarer

--- main.cpp
+++ main.cpp
@@ -15,14 +15,25 @@ using namespace std;
 
 int main(void)
 {
+	//programmet kan tage lidt tid da der er sleep kommandoer
+	//cout linjerne er for at se at programmet kører
+
 	cout << "program starting" << endl;
 
+	//file pointer
 	FILE *fs;
 
+	//absolut sti til gpio registret der bliver brugt
 	fs = fopen("/sys/devices/platform/ocp/4804c000.gpio/gpiochip1/gpio/gpio59/direction", "w");
 	cout << "file opened" << endl;
+
+	//fseek for at sætte pointeren tilbage til start i filen
 	fseek(fs,0,SEEK_SET);
+
+	//fprintf for at sætte en ny værdi ind i registret/filen
 	fprintf(fs,"out");
+
+	//fclose for at gøre den klar til næste fil der skal sættes
 	fclose(fs);
 
 
@@ -35,6 +46,9 @@ int main(void)
 	sleep(2);
 	while(1)
 	{
+		//for hver ny værdi der bliver indsat ind i den samme fil,
+		//skal pointeren sættes tilbage til starten af filen med evt. fseek
+
 		fseek(fs,0,SEEK_SET);
 		fprintf(fs,"1");
 		cout << "on" << endl;
